527823d4e92722a230f86ea12fb1c2c2c1e67b77
Saadia Asghar <saadianigah@gmail.com>
2026-07-13 10:08:10 +0500

    Use ESM imports in the Vercel API function

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,30 +1,16 @@
 import express from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
-import router from "./routes";
-import { logger } from "./lib/logger";
+import router from "./routes/index.js";
+import { logger } from "./lib/logger.js";
 
 const app = express();
 
-app.use(
-  pinoHttp({
-    logger,
-    serializers: {
-      req(req: any) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res(res: any) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
-    },
-  }),
-);
+app.use((req, res, next) => {
+  res.on("finish", () => {
+    logger.info({ method: req.method, statusCode: res.statusCode, url: req.path }, "Request completed");
+  });
+  next();
+});
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
